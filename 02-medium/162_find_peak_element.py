7b5c6ce17a8effdea15a9f08d3dbd8c1c6703d4b
Lesley <tomosadalesley@gmail.com>
2023-05-25 21:38:09 -0700

    completed 2 easy

--- 02-medium/162_find_peak_element.py
+++ 02-medium/162_find_peak_element.py
@@ -33,15 +33,15 @@
 # -231 <= nums[i] <= 231 - 1
 # nums[i] != nums[i + 1] for all valid i.
 
-def findPeakElement(nums):
-    left, right = 0, len(nums) - 1
-
-    while left < right:
-        mid = (left + right) // 2
-
-        if nums[mid] > nums[mid + 1]:
-            right = mid
-        else:
-            left = mid + 1
-
-    return left
+class Solution:
+    def findPeakElement(self, nums):
+        low = 0
+        high = len(nums)-1
+        while low <= high:
+            mid = low+(high-low)//2
+            if mid > 0 and nums[mid] < nums[mid-1]:
+                high = mid-1
+            elif mid < len(nums)-1 and nums[mid] < nums[mid+1]:
+                low = mid+1
+            else:
+                return mid
