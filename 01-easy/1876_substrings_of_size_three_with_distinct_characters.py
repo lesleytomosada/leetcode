957927d999816fdb5af0a3d48d0c50496d7d59e4
Lesley <tomosadalesley@gmail.com>
2023-06-05 18:19:16 -0700

    completed two easy

---
+++ 01-easy/1876_substrings_of_size_three_with_distinct_characters.py
@@ -0,0 +1,52 @@
+# 1876. Substrings of Size Three with Distinct Characters
+# Easy
+# 1.2K
+# 30
+# Companies
+# A string is good if there are no repeated characters.
+
+# Given a string s​​​​​, return the number of good substrings of length three
+# in s​​​​​​.
+
+# Note that if there are multiple occurrences of the same substring, every
+# occurrence should be counted.
+
+# A substring is a contiguous sequence of characters in a string.
+
+
+# Example 1:
+
+# Input: s = "xyzzaz"
+# Output: 1
+# Explanation: There are 4 substrings of size 3: "xyz", "yzz", "zza", and "zaz"
+# The only good substring of length 3 is "xyz".
+# Example 2:
+
+# Input: s = "aababcabc"
+# Output: 4
+# Explanation: There are 7 substrings of size 3: "aab", "aba", "bab", "abc",
+# "bca", "cab", and "abc".
+# The good substrings are "abc", "bca", "cab", and "abc".
+
+
+# Constraints:
+
+# 1 <= s.length <= 100
+# s​​​​​​ consists of lowercase English letters.
+
+class Solution:
+    def countGoodSubstrings(self, s: str) -> int:
+        output = 0
+        for i in range(len(s)-2):
+            substring = s[i:i+3]
+            if len(substring) == len(set(substring)):
+                output += 1
+
+        return output
+
+    def countGoodSubstrings2(self, s: str) -> int:
+        ans = 0
+        for i in range(len(s)-2):
+            if len(set(s[i:i+3])) == 3:
+                ans += 1
+        return ans
